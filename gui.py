9e93553bd09a73a04de55de866657a41522fc203
Tomasz Lasota <tomasz.lasota@lexdiagnostics.com>
2024-01-14 00:13:01 +0000

    Refactor: included read serial and plot data functions

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from datetime import datetime
 from sensor import SerialCommunication
+import serial.tools.list_ports as stlp
 
 
 
@@ -18,23 +19,44 @@ class SerialPlotterApp:
         self.com_port_var = tk.StringVar()
         self.serial_communication = SerialCommunication()
 
-        # GUI Components
+        # Initilize GUI Components
         self.create_widgets()
-
+    
+    # get all active COM ports
+    def get_active_com(self):
+        self.available_ports = [port.device for port in stlp.comports()]
+        return self.available_ports
+    
+    def current_pressure(self):
+        self.value = self.serial_communication.read_serial_data()
+        self.s_current_value.config(text= self.value)
+        self.s_current_value.after(10, self.current_pressure) # update the value every 10 miliseconds
+    
+    # create the GUI components
     def create_widgets(self):
         # COM Port Selection
         com_label = ttk.Label(self.root, text="Select COM Port:")
         com_label.grid(row=0, column=0, padx=10, pady=10)
 
+        # dropdown menu
         com_combobox = ttk.Combobox(self.root, textvariable=self.com_port_var)
         com_combobox.grid(row=0, column=1, padx=10, pady=10)
-        com_combobox['values'] = [f"COM{x}" for x in range(1, 10)]
+        com_combobox['values'] = self.get_active_com()
+
+        # Current sensor value label
+        sensor_label = ttk.Label(self.root, text="Current Sensor Value:")
+        sensor_label.grid(row=0, column=2, padx=10, pady=5)
+
+        # Current sensor value
+        self.s_current_value = ttk.Label(self.root, text="0.000", font=("Helvetica", 16))
+        self.s_current_value.grid(row=1, column=2, padx=10, pady=5, sticky="n")
+        self.current_pressure()
 
         # Plotting
         self.fig = Figure(figsize=(6, 4), dpi=100)
         self.ax = self.fig.add_subplot(1, 1, 1)
-        self.ax.set_xlabel('Time')
-        self.ax.set_ylabel('Value')
+        self.ax.set_xlabel('Time (s)')
+        self.ax.set_ylabel('Pressure (mbar)')
         self.canvas = FigureCanvasTkAgg(self.fig, master=self.root)
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.grid(row=1, column=0, columnspan=2, padx=10, pady=10)
@@ -46,27 +68,29 @@ class SerialPlotterApp:
         stop_button = ttk.Button(self.root, text="Stop", command=self.stop_plotting)
         stop_button.grid(row=2, column=1, pady=10)
 
+    def read_and_plot_data(self):
+        time, value = self.serial_communication.read_serial_data()
+        self.ax.clear()
+        self.ax.plot(time, value)
+        self.root.update_idletasks()
+        self.canvas.draw()
+        self.root.after(100, self.read_and_plot_data) # triggers the function every 10 miliseconds
+    
     def start_plotting(self):
         com_port = self.com_port_var.get()
 
         try:
             self.serial_communication.open_serial_port(com_port)
-            self.root.after(100, self.read_and_plot_data)
+            self.read_and_plot_data()
         except serial.SerialException as e:
             print(e)  # You can handle errors as appropriate
-
-    def read_and_plot_data(self):
-        self.serial_communication.read_serial_data()
-        self.plot_data()
-        self.root.update_idletasks()
-        self.root.after(100, self.read_and_plot_data)
 
     def stop_plotting(self):
         self.serial_communication.close_serial_port()
         self.save_to_csv()
 
     def save_to_csv(self):
-        filename = f"data_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
+        filename = f"data_{datetime.now().strftime('%Y%m%d %H%M%S')}.csv"
         self.serial_communication.save_to_csv(filename)
 
 if __name__ == "__main__":
--- sensor.py
+++ sensor.py
@@ -4,11 +4,12 @@ from matplotlib.animation import FuncAnimation
 import csv
 from datetime import datetime
 from time import strftime
+import time
 
 
 class SerialCommunication:
     def __init__(self):
-        self.ser = None
+        self.ser = serial.Serial()
         self.data = []
 
     def open_serial_port(self, com_port):
@@ -19,11 +20,20 @@ class SerialCommunication:
 
     def read_serial_data(self):
         try:
-            line = self.ser.readline().decode('utf-8').strip()
-            if line:
-                timestamp = datetime.now().strftime('%H:%M:%S')
-                value = float(line)
-                self.data.append((timestamp, value))
+            if self.ser.isOpen():
+                self.ser.flushOutput()
+                self.line = self.ser.readline().decode('utf-8').strip()
+                if self.line:
+                    self.timestamp = datetime.now().strftime('%H:%M:%S')
+                    while "\\n" not in str(self.line):
+                        self.temp = self.ser.readline()
+                        if self.temp.decode():
+                            self.data_read = (self.line.decode() + self.temp.decode()).encode()
+                        self.data_read = self.data_read.decode().strip()
+                    self.data_values = self.data_read.split(", ")
+                    self.value = float(self.data_values[4])
+                    self.data.append((self.timestamp, self.value))
+                    return self.timestamp, self.value
         except serial.SerialException as e:
             raise serial.SerialException(f"Error reading from serial port: {e}")
 
@@ -33,6 +43,6 @@ class SerialCommunication:
 
     def save_to_csv(self, filename):
         with open(filename, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Timestamp', 'Value'])
-            csv_writer.writerows(self.data)
+            self.csv_writer = csv.writer(csvfile)
+            self.csv_writer.writerow(['Time', 'Value'])
+            self.csv_writer.writerows(self.data)
